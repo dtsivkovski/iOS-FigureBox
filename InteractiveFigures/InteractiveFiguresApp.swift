75ae17c5f5478451c328b318e01df73fc7a650ef
dtsivkovski <dtsivkovs@gmail.com>
2025-04-20 18:06:51 -0700

    Initial Commit

---
+++ InteractiveFigures/InteractiveFiguresApp.swift
@@ -0,0 +1,17 @@
+//
+//  InteractiveFiguresApp.swift
+//  InteractiveFigures
+//
+//  Created by Daniel Tsivkovski on 4/20/25.
+//
+
+import SwiftUI
+
+@main
+struct InteractiveFiguresApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ InteractiveFiguresTests/InteractiveFiguresTests.swift
@@ -0,0 +1,17 @@
+//
+//  InteractiveFiguresTests.swift
+//  InteractiveFiguresTests
+//
+//  Created by Daniel Tsivkovski on 4/20/25.
+//
+
+import Testing
+@testable import InteractiveFigures
+
+struct InteractiveFiguresTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
